d2614be90fd07ca1ee7235fc938867b42987fdf3
NikolasK88 <maxsuccess4@gmail.com>
2025-10-04 12:09:10 +0300

    Add 3rd lesson

---
+++ lesson_3.py
@@ -0,0 +1,120 @@
+# Data types
+
+# Незмінні
+
+# Numeric
+
+# int()       #Integer
+# float()     # 1.5
+# #decimal    # 1.5 більш точний
+# complex()   # (1 + 2j)
+#
+# str()       # String(рядок)
+#
+#
+# bool()      # Boolean (True/False) 1/0
+#
+# None        # NoneType
+#
+# tuple()     #() кортеж
+#
+# # Змінні
+#
+# list()      #[] список
+# dict()      #{} словник ключ значення
+# set()       #{} множина
+
+
+
+
+# num_1 = 2
+#
+# print(num_1, id(num_1), type(num_1))
+#
+# num_2 = num_1
+# print(num_2, id(num_2), type(num_2))
+#
+# num_1 = 5
+#
+# print(num_1, id(num_1), type(num_1))
+
+# PEP8
+# word_new_word # snake case
+# wordNewWord # camel case
+
+# worked_days = 1
+# days = 2
+# result = worked_days + days
+
+
+
+
+# list_num_1 = [1, 2, 3]
+#
+# list_num_2 = list_num_1
+#
+# list_num_1.append(4)
+#
+#
+# print("list_num_1", list_num_1)
+# print("list_num_2", list_num_2)
+
+
+# num_1 = 9
+# num_2 = 2
+#
+# result_add = num_1 + num_2 # +
+# result_sub = num_1 - num_2 # -
+#
+# result_div_1 = num_1 / num_2 # /
+# result_div_2 = num_1 // num_2 # // цілочислене ділення
+# result_div_3 = num_1 % num_2 # % залишок на ділення ділення
+# result_div_4 = divmod(num_1, num_2) # цілочислене та залишок на ділення
+#
+# result_mul = num_1 * num_2 # *
+# result_power = num_1 ** num_2 # ступінь
+# result_root = num_1 ** 0.5 # корінь
+#
+#
+# print(result_root, type(result_root))
+
+
+# my_str = "text"
+# my_str = 'text'
+# my_str = """text"""
+# my_str = '''text'''
+#
+# my_text = "Hi I'mNick, \"Kyiv\"" # екранування \, \n, \t
+# # my_text = '''Hi I'm Nick, "Kyiv" '''
+#
+# print(my_text)
+
+
+# name = "Denis"
+# age = 18
+#
+# # result = f"I'm {name}, I'm {age} years old" #format string
+# result = "I'm " + name + ", I'm " + str(age) + " years old"   # конкатинація
+# print(result)
+
+
+# num_1 = "14.0"
+# num_2 = 5
+#
+# result = float(num_1) + num_2
+# print(result)
+
+# int 0123456789
+# float 0123456789.
+
+
+# print("Hello")
+#
+# type()
+# id()
+#
+# phone_type
+# my_print
+# goods_id
+
+
