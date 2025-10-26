4d22b1bd07475ec5e9a66b5756b43a14e038cca2
NikolasK88 <maxsuccess4@gmail.com>
2025-10-26 10:58:18 +0200

    Add 4th lesson

---
+++ lesson_4.py
@@ -0,0 +1,129 @@
+
+# Data type
+
+# str
+
+# float
+# decimal
+# int
+# complex
+
+# bool
+# NoneType None
+
+# tuple
+
+# list
+# dict
+# set
+
+
+
+
+# None
+
+# monthes_in_year = 12
+
+# print("Hello")
+# id(monthes_in_year)
+
+
+# value_id = id(monthes_in_year)
+# value_print = print(monthes_in_year)
+#
+# print(value_print)
+
+
+
+# Boolean
+
+# is_confirmed = True
+# is_done = False
+
+# value_int = -111
+# value_float = 0.00000001
+# value_str = ""
+# print(bool(value_str))
+
+# value_int = 40
+# print(bool(value_int) * 10)
+
+################################ Logic operators >, <, >=, <=, ==, !=, in, is, not ################################################
+
+# value_int_1 = 100
+# value_int_2 = 10
+#
+# print(value_int_1 != value_int_2)
+
+# value_str_1 = "Hello"
+# value_str_2 = "Helo"
+#
+# print(value_str_1 > value_str_2)
+
+# print(value_str_2 in value_str_1)
+# print(value_str_2 not in value_str_1)
+
+# print(not bool(value_str_2)) # not лдя того щоб перевернути значення
+
+
+# is ===
+# value_int_1 = 10
+# value_int_2 = 10.0
+#
+# print(value_int_1 == value_int_2)
+# print(value_int_1 is value_int_2)
+
+# if щось тру(правда):
+#     роби це
+
+
+#  and or
+
+# value = 100
+
+# if value > 0 or value <= 10:
+# if value > 0 and value <= 10:
+
+# if value > 0 and value <= 10:
+# if 0 < value <= 10:
+#     print(f"{value} is bigger than 0")
+# elif value == 0: # else if
+#     print(f"{value} equal 0")
+# elif value > 10:
+#     print(f"{value} is bigger than 10")
+# else:
+#     print(f"{value} is less than 0")
+
+
+
+# if value:
+#     print(value)
+
+# print("end")
+
+######### тернарний оператор short hand if #######
+
+# value_int_1 = 5
+# # value_int_2 = 2
+# #
+# # if value_int_1 > value_int_2: print("value_int_1 is greater than value_int_2")
+#
+# value_int_1 = 200000
+# value_int_2 = 330
+#
+# # if value_int_1 > value_int_2:
+# #     print("A")
+# # else:
+# #     print("B")
+#
+# print("A") if value_int_1 > value_int_2 else print("B")
+
+
+
+
+
+
+
+
+
+
