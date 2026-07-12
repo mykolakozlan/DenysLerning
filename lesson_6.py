56331d6c4248417cfda2dfdbede6aa71cb718f0f
NikolasK88 <maxsuccess4@gmail.com>
2026-07-12 18:29:25 +0300

    Add 6th lesson

---
+++ lesson_6.py
@@ -0,0 +1,91 @@
+
+####### List #######
+
+# num_1 = 4
+#
+# my_list = [1, True, "hello", 0.4, [True, False], num_1]
+
+# list_len = len(my_list)
+# print(list_len)
+
+# my_list[-1]("fffff")
+
+# print(my_list.count(1))
+
+# num_1, num_2, *temp1 = [1, 4, 5, 9, 0]
+
+# print(num_1, num_2, temp1)
+
+
+
+
+
+# ############################################
+
+# base_list = [1, 2, 3]
+# my_new_list = base_list * 4
+# my_new_list = [id, id, id ...]
+
+
+# print(my_new_list)
+# base_list[0] = 10
+#
+# print(f"base_list {base_list}")
+# print(f"my_new_list {my_new_list}")
+
+# # # ######################################
+
+# from copy import deepcopy
+# #
+# base_list = [1, 2, 3, [True, False]]
+# # base_list = [id, id, id, link] #copy
+# # base_list = [id, id, id, link.copy()] #deepcopy
+#
+#
+# # my_new_list = [base_list[:]] * 4
+# my_new_list = [base_list.copy()] * 4
+# # my_new_list = [deepcopy(base_list)] * 4
+#
+# # my_new_list = [link, link, link ...]
+# print(my_new_list)
+#
+#
+# base_list[-1][0] = "Hello"
+#
+# print(f"base_list {base_list}")
+# print(my_new_list)
+
+# # # ######################################
+
+
+
+
+# numbers = ["How are you ?", "By", "Hello"]
+#
+# if len(numbers) <= 1:
+#     print(numbers)
+#
+# elif len(numbers) >= 1:
+#     numbers_1 = numbers[-1]
+#     numbers.insert(0, numbers_1)
+#     numbers.pop() # pop повертає видалене значення
+#     print(numbers)
+
+# numbers = ["How are you ?", "By", "Hello"]
+#
+# if len(numbers) <= 1:
+#     print(numbers)
+#
+# elif len(numbers) >= 1:
+#     numbers.insert(0, numbers.pop())
+
+
+
+
+
+
+
+
+
+
+
