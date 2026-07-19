7e6d6382531fa2cb6fd329bc1edde078c5b4a7b3
NikolasK88 <maxsuccess4@gmail.com>
2026-07-19 17:08:50 +0300

    Add 9th lesson

---
+++ lesson_9.py
@@ -0,0 +1,77 @@
+
+
+########### Strings ##########
+
+# value_str = "Hello"
+#
+# print(value_str, id(value_str))
+#
+# value_str = "hello"
+#
+# print(value_str, id(value_str))
+
+
+# value_str = "Hello"
+# value_str = 'Hello I\'m\n'
+# value_str = '''Hello'''
+# value_str = """Hello"""
+
+# \n, \, \t
+
+
+# name = "Nick"
+# age = 24
+#
+# # value_str = "Hello " + "I'm " + name + " " + str(age)
+# value_str = f"Hello I'm {name}, {age}"
+#
+# print(value_str)
+
+# next() -> ('N', True), ('i', True), ('c', True), ('k', False)
+
+# for letter in name:
+#     print(letter)
+
+# name = "Nick"
+
+# print(name[0])
+# print(name[22])
+# print(name[:2])
+# print(name[2:])
+# print(name[22:84], type(name[22:84]))
+# print(name[-1])
+
+
+# email = "test@gmail.com"
+# email = "Test@GMAIL.com"
+
+# first_name = "nick"
+# ful_name = "Nick DEV"
+#
+#
+# # value_lower = email.lower()
+# # value_upper = email.upper()
+# # value_capital = first_name.capitalize()
+# # value_capital = ful_name.title()
+#
+# value_swap = ful_name.swapcase() #шоби шо?
+#
+# print(ful_name)
+# print(value_swap)
+
+# is_confirmed = True/False
+# is_new = True/False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
