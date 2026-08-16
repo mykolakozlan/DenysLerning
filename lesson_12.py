4de2f9ae6a6ca4a9740a64d288247764f074c354
NikolasK88 <maxsuccess4@gmail.com>
2026-08-16 08:04:24 +0300

    Add 12th lesson

---
+++ lesson_12.py
@@ -0,0 +1,80 @@
+#
+# original_name = input(str(" Hi, my friend! I’ll help you encrypt the text. Please enter the name: "))
+#
+# sym_encode_ord = 0
+# sym_encode_chr = ""
+# sym_decode_ord = 0
+# sym_decode_chr = ""
+#
+# encrypted_name = ""
+# decrypted_name = ""
+#
+# for sym in original_name:
+#     sym_encode_ord = ord(sym) + 100
+#     sym_encode_chr = chr(sym_encode_ord)
+#     encrypted_name += str(sym_encode_chr)
+#
+# print(encrypted_name)
+#
+# ok = input(str("Do you want to decrypt the name? If yes, enter yes. If not, enter no: "))
+#
+# if ok == "yes":
+#
+#     for enc_sym in encrypted_name:
+#         sym_decode_ord = ord(enc_sym) - 100
+#         sym_decode_chr = chr(sym_decode_ord)
+#         decrypted_name += str(sym_decode_chr)
+#
+#     print(decrypted_name)
+# else:
+#     print(f" Then let’s leave it encrypted:   {encrypted_name}")
+#
+#
+#
+#
+
+
+# original_name = input(str(" Hi, my friend! I’ll help you encrypt the text. Please enter the name: "))
+#
+# sym_encode_chr = ""
+# sym_decode_chr = ""
+#
+# encrypted_name = ""
+# decrypted_name = ""
+#
+# for sym in original_name:
+#     sym_encode_chr = chr(ord(sym) + 100)
+#     encrypted_name += str(sym_encode_chr)
+#
+# print(encrypted_name)
+#
+# ok = input(str("Do you want to decrypt the name? If yes, enter yes. If not, enter no: "))
+#
+# if ok == "yes":
+#
+#     for enc_sym in encrypted_name:
+#         sym_decode_chr = chr(ord(enc_sym) - 100)
+#         decrypted_name += str(sym_decode_chr)
+#
+#     print(decrypted_name)
+# else:
+#     print(f" Then let’s leave it encrypted:   {encrypted_name}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
